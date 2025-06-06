345d7fd4beec7caf619b72de049d3c37fc6abf89
Asoo <asoo.fatahi@gmail.com>
2025-06-06 11:42:15 +0430

    NSP-20:upload load stating for all pages project

---
+++ Endpoint_WebApp/wwwroot/js/Load_Stating.js
@@ -0,0 +1,12 @@
+﻿
+
+
+window.addEventListener('load', function () {
+    setTimeout(function () {
+        const loader = document.getElementById('site-loader');
+        if (loader) {
+            loader.style.opacity = '0';
+            setTimeout(() => loader.style.display = 'none', 600);
+        }
+    }, 2500); // مدت زمان نمایش لودر (۲.۵ ثانیه)
+});
