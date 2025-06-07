322697784c9052a35119efa26d86b7d70cdac720
Asoo <asoo.fatahi@gmail.com>
2025-06-08 00:51:56 +0430

    NSP-27: Complete admin dashboard page and Completing the admin page layout

--- Endpoint_WebApp/Controllers/HomeController.cs
+++ Endpoint_WebApp/Controllers/HomeController.cs
@@ -18,10 +18,6 @@ namespace Endpoint_WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
