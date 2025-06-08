e012f3135bfb3dcec18494887574c883d05284d5
mhmdpnhi <1ki.mp.ng8@gmail.com>
2025-06-08 12:56:03 +0330

    NSP-12: The ReportPage has been implemented.

--- Endpoint_WebApp/Controllers/AdminDashboardController.cs
+++ Endpoint_WebApp/Controllers/AdminDashboardController.cs
@@ -1,34 +1,120 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Endpoint_WebApp.Models.ViewModels.AdminDashboard;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Nursing_Service.Application.Services.Nurse.Query.GetNurses;
+using Nursing_Service.Application.Services.Patient.Query.GetPatients;
+using Nursing_Service.Application.Services.PatinetNeedService.Query.GetPatientNeedServices;
+using Nursing_Service.Application.Services.Service.Query.GetServices;
+using Nursing_Service.Application.Services.SuperVisor.Query.GetSuperVisors;
+using Nursing_Service.Application.Services.Users.Query;
 
 namespace Endpoint_WebApp.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminDashboardController : Controller
     {
-        public IActionResult NeedServices()
+        private readonly IGetPatients _getPatients;
+        private readonly IGetUserService _getUser;
+        private readonly IGetNursesService _getNurses;
+        private readonly IGetPatientNeedServices _getPatientNeedServices;
+        private readonly IGetServices _getServices;
+        private readonly IGetSuperVisors _getSuperVisorService;
+
+        public AdminDashboardController(IGetPatients getPatients, IGetUserService getUser, IGetNursesService getNurses, IGetPatientNeedServices getPatientNeedServices, IGetServices getServices, IGetSuperVisors getSuperVisorService)
         {
-            return View();
+            _getPatients = getPatients;
+            _getUser = getUser;
+            _getNurses = getNurses;
+            _getPatientNeedServices = getPatientNeedServices;
+            _getServices = getServices;
+            _getSuperVisorService = getSuperVisorService;
         }
 
-        public IActionResult Nurses()
+        public async Task<IActionResult> Reports()
         {
-            return View();
+            ViewData["PageTitle"] = "گزارشات";
+
+            var nurseResult = await _getNurses.ExcuteAsync();
+            ulong? numberOfNurses = 0;
+            if (nurseResult.IsSuccess is true)
+                numberOfNurses = (ulong)nurseResult?.Data?.Count;
+
+            var patientsResult = await _getPatients.ExcuteAsync();
+            ulong? numberOfPatients = 0;
+            if (patientsResult.IsSuccess is true)
+                numberOfPatients = (ulong)patientsResult?.Data?.Count;
+
+            var servicesResult = await _getServices.ExcuteAsync();
+            ulong? numberOfServices = 0;
+            if (servicesResult.IsSuccess is true)
+                numberOfServices = (ulong)servicesResult?.Data?.Count;
+
+            var pnssResult = await _getPatientNeedServices.ExcuteAsync();
+            ulong? numberOfPatientNeedServicesInMonth = 0;
+            if (pnssResult.IsSuccess is true)
+                numberOfPatientNeedServicesInMonth = (ulong)(pnssResult.Data?.Where(pns => pns.ServiceDateTime.Month == DateTime.Now.Month).Count());
+
+            return View(new ReportsViewModel
+            {
+                NumberOfNurses = numberOfNurses,
+                NumberOfPatients = numberOfPatients,
+                NumberOfPNSInThisMonth = numberOfPatientNeedServicesInMonth,
+                NumberOfServices = numberOfServices
+            });
         }
 
-        public IActionResult Patients()
+        public async Task<IActionResult> Operators()
         {
-            return View();
+            var operators = await _getUser.ExcuteAsync();
+
+            ViewData["PageTitle"] = "اپراتورها";
+
+            return View(operators.Data);
         }
 
-        public IActionResult Services()
+        public async Task<IActionResult> NeedServices()
         {
-            return View();
+            var pnss = await _getPatientNeedServices.ExcuteAsync();
+
+            ViewData["PageTitle"] = "لیست درخواست‌ها";
+
+            return View(pnss.Data);
         }
 
-        public IActionResult SuperVisors()
+        public async Task<IActionResult> Nurses()
         {
-            return View();
+            var nurses = await _getNurses.ExcuteAsync();
+
+            ViewData["PageTitle"] = "پرستاران";
+
+            return View(nurses.Data);
+        }
+
+        public async Task<IActionResult> Patients()
+        {
+            var patients = await _getPatients.ExcuteAsync();
+
+            ViewData["PageTitle"] = "بیماران";
+
+            return View(patients.Data);
+        }
+
+        public async Task<IActionResult> Services()
+        {
+            var services = await _getServices.ExcuteAsync();
+
+            ViewData["PageTitle"] = "مدیریت سرویس‌ها";
+            
+            return View(services.Data);
+        }
+
+        public async Task<IActionResult> SuperVisors()
+        {
+            var supervisors = await _getSuperVisorService.ExcuteAsync();
+
+            ViewData["PageTitle"] = "سوپروایزرها";
+
+            return View(supervisors.Data);
         }
     }
 }
---
+++ Endpoint_WebApp/Models/ViewModels/AdminDashboard/ReportsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Endpoint_WebApp.Models.ViewModels.AdminDashboard
+{
+    public class ReportsViewModel
+    {
+        public ulong? NumberOfPatients { get; set; }
+        public ulong? NumberOfNurses { get; set; }
+        public ulong? NumberOfServices { get; set; }
+        public ulong? NumberOfPNSInThisMonth { get; set; }
+    }
+}
