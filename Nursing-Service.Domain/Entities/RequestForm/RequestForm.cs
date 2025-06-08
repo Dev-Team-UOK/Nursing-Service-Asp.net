e160000deadf38c481c27fc8ec8ab9c4c47c850c
mhmdpnhi <1ki.mp.ng8@gmail.com>
2025-06-08 03:45:13 +0330

    NSP-23: RequestForm entity has been added.

---
+++ Nursing-Service.Domain/Entities/RequestForm/RequestForm.cs
@@ -0,0 +1,12 @@
+﻿using Nursing_Service.Domain.Entities.Base;
+
+namespace Nursing_Service.Domain.Entities.RequestForm
+{
+    public class RequestForm : BaseEntity
+    {
+        public required string FullName { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required string Address { get; set; }
+        public string Name { get; set; }
+    }
+}
